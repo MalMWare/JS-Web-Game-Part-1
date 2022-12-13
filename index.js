771d83ad7c6d99612407c72bdc25b0d7766dc4d9
MalMWare <malkelleymartin@gmail.com>
2022-12-13 03:34:06 -0800

    clickable items

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(image, bottom, left) { 
+    let newImage = document.createElement('img')
+    newImage.src = image
+    newImage.style.position = 'fixed'
+    newImage.style.left = left
+    newImage.style.bottom = bottom
+    document.body.append(newImage)
+    return newImage
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+let greenCharacter = newImage('assets/green-character.gif', '100px', '100px' )
+let pineTree = newImage('assets/pine-tree.png', '200px', '450px' )
+let tree = newImage('assets/tree.png', '300px', '200px')
+let pillar = newImage('assets/pillar.png', '100px', '350px')
+let crate = newImage('assets/crate.png', '200px', '150px')
+let well = newImage('assets/well.png', '425px', '500px')
+
+function newItem(image, bottom, left) { 
+    let newItem = newImage(image, bottom, left)
+    newItem.addEventListener('dblclick', function(){
+        newItem.remove()
+    })
+    return newItem   
+} 
+
+let sword = newItem('assets/sword.png', '405px', '500px')
+let shield = newItem('assets/sheild.png', '185px', '165px')
+let staff = newItem('assets/staff.png', '100px', '600px')
+
+//let grass = newImage('assets/grass.png', 'window.innerWidth 100px', 'window.innerHeight')
+
+
